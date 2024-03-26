d658042744b76d966365fab672bd85ed1cb45f41
AleGxrcia <125775730+AleGxrcia@users.noreply.github.com>
2024-03-26 17:25:27 -0400

    Add project files.

---
+++ RealEstateApp.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealEstateApp.WebApp.Models;
+using System.Diagnostics;
+
+namespace RealEstateApp.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ RealEstateApp.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RealEstateApp.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
