48d14a91f78749c076e9b8c0ca603ec4ff1d8c58
frankjrx2004@gmail.com <eljunior122rd@gmail.com>
2024-03-27 18:38:15 -0400

    adding ViewModels

---
+++ RealEstateApp.Core.Application/ViewModels/Property/PropertyViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstateApp.Core.Application.ViewModels.Property
+{
+	public class PropertyViewModel
+	{
+		public int? Id { get; set; }
+		public string PropertyType { get; set; }
+		public int Code { get; set; }
+		public string SalesType { get; set; }
+		public decimal Price { get; set; }
+		public decimal LandSize { get; set; }
+		public int NumberOfRooms { get; set; }
+		public int NumberOfBathrooms { get; set; }
+
+		public string? ImgUrl { get; set; }
+	}
+}
