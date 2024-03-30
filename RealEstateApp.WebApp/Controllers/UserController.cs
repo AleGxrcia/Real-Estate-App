4e01be6661f4a21db5ffadf0e0784000576ee278
AleGxrcia <125775730+AleGxrcia@users.noreply.github.com>
2024-03-30 15:24:51 -0400

    Add user agents maintenance functionality

---
+++ RealEstateApp.WebApp/Controllers/UserController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealEstateApp.Core.Application.Dtos.Account;
+using RealEstateApp.Core.Application.Enums;
+using RealEstateApp.Core.Application.Interfaces.Services;
+
+namespace RealEstateApp.WebApp.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public async Task<IActionResult> Agent()
+        {
+            var users = await _userService.GetAllUsers();
+            var agents = users.Select(a => a.Role == Roles.Agent.ToString()).ToList();
+
+            return View("Agent", agents);
+        }
+
+        public async Task<IActionResult> ConfirmUserStatus(string id)
+        {
+            UserResponse user = await _userService.getUserByIdAsync(id);
+            return View("ConfirmUserStatus", user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ActiveUser(UserResponse vm)
+        {
+            UserRequest userRequest = new()
+            {
+                Id = vm.Id,
+            };
+
+            var result = await _userService.ActiveUser(userRequest.Id);
+            return RedirectToRoute(new { controller = "User", action = "Agent" });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> InactiveUser(UserResponse vm)
+        {
+            UserRequest userRequest = new()
+            {
+                Id = vm.Id,
+            };
+
+            var result = await _userService.InactiveUser(userRequest.Id);
+            return RedirectToRoute(new { controller = "User", action = "Agent" });
+        }
+    }
+}
