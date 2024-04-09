bc398defc17fd80c17004fa846089b2cdb46d747
AleGxrcia <125775730+AleGxrcia@users.noreply.github.com>
2024-04-09 13:50:07 -0400

    Add Admin dashboard

---
+++ RealEstateApp.Core.Application/ViewModels/Dashboard/AdminDashboardViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace RealEstateApp.Core.Application.ViewModels.Dashboard
+{
+    public class AdminDashboardViewModel
+    {
+        public int TotalActiveAgents { get; set; }
+        public int TotalActiveAdmins { get; set; }
+        public int TotalActiveDevs { get; set; }
+        public int TotalInactiveAgents { get; set; }
+        public int TotalInactiveAdmins { get; set; }
+        public int TotalInactiveDevs { get; set; }
+        public int TotalPropertiesRegistered { get; set; }
+    }
+}
---
+++ RealEstateApp.WebApp/Controllers/AdminController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealEstateApp.Core.Application.Enums;
+using RealEstateApp.Core.Application.Interfaces.Services;
+using RealEstateApp.Core.Application.ViewModels.Dashboard;
+
+namespace RealEstateApp.WebApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IUserService _userService;
+        private readonly IPropertyService _propertyService;
+
+        public AdminController(IUserService userService, IPropertyService propertyService)
+        {
+            _userService = userService;
+            _propertyService = propertyService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userService.GetAllUsers();
+            var properties = await _propertyService.GetAllViewModel();
+
+            var dashboardVm = new AdminDashboardViewModel()
+            {
+                TotalActiveAgents = users.Where(u => u.IsActive == true).Select(a => a.Role == Roles.Agent.ToString()).Count(),
+                TotalInactiveAgents = users.Where(u => u.IsActive == false).Select(a => a.Role == Roles.Agent.ToString()).Count(),
+                TotalActiveAdmins = users.Where(u => u.IsActive == true).Select(a => a.Role == Roles.Admin.ToString()).Count(),
+                TotalInactiveAdmins = users.Where(u => u.IsActive == false).Select(a => a.Role == Roles.Admin.ToString()).Count(),
+                TotalActiveDevs = users.Where(u => u.IsActive == true).Select(a => a.Role == Roles.Developer.ToString()).Count(),
+                TotalInactiveDevs = users.Where(u => u.IsActive == false).Select(a => a.Role == Roles.Developer.ToString()).Count(),
+                TotalPropertiesRegistered = properties.Count()
+            };
+
+            return View(dashboardVm);
+        }
+    }
+}
