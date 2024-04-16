5acbe6546a68d2278951d838de14952e15dcc265
AleGxrcia <125775730+AleGxrcia@users.noreply.github.com>
2024-04-16 10:59:21 -0400

    Fix deletion behavior for related entities

---
+++ RealEstateApp.Infrastructure.Persistence/Migrations/20240416114447_UpdateRelationships.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RealEstateApp.Infrastructure.Persistence.Migrations
+{
+    public partial class UpdateRelationships : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FavoriteProperties_Properties_PropertyId",
+                table: "FavoriteProperties");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ImprovementProperties_Properties_PropertyId",
+                table: "ImprovementProperties");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FavoriteProperties_Properties_PropertyId",
+                table: "FavoriteProperties",
+                column: "PropertyId",
+                principalTable: "Properties",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ImprovementProperties_Properties_PropertyId",
+                table: "ImprovementProperties",
+                column: "PropertyId",
+                principalTable: "Properties",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FavoriteProperties_Properties_PropertyId",
+                table: "FavoriteProperties");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ImprovementProperties_Properties_PropertyId",
+                table: "ImprovementProperties");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FavoriteProperties_Properties_PropertyId",
+                table: "FavoriteProperties",
+                column: "PropertyId",
+                principalTable: "Properties",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ImprovementProperties_Properties_PropertyId",
+                table: "ImprovementProperties",
+                column: "PropertyId",
+                principalTable: "Properties",
+                principalColumn: "Id");
+        }
+    }
+}
---
+++ RealEstateApp.Infrastructure.Persistence/Migrations/20240416115810_UpdateImprovementPropertiesRelationship.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RealEstateApp.Infrastructure.Persistence.Migrations
+{
+    public partial class UpdateImprovementPropertiesRelationship : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ImprovementProperties_Improvements_ImprovementId",
+                table: "ImprovementProperties");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ImprovementProperties_Improvements_ImprovementId",
+                table: "ImprovementProperties",
+                column: "ImprovementId",
+                principalTable: "Improvements",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ImprovementProperties_Improvements_ImprovementId",
+                table: "ImprovementProperties");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ImprovementProperties_Improvements_ImprovementId",
+                table: "ImprovementProperties",
+                column: "ImprovementId",
+                principalTable: "Improvements",
+                principalColumn: "Id");
+        }
+    }
+}
